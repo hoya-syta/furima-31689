72f069dfe2ae2acc1063de239a715890ea49acd4
hoya-syta <erukhnjhgfd@gmail.com>
2020-11-23 19:05:50 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,21 @@
 class Item < ApplicationRecord
 
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_explain
+    validates :price,format: { with: /\A[0-9]+\z/ },
+                     length: { in: 300..9999999 }
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :delivery_fee_id
+      validates :delivery_zone_id
+      validates :delivery_time_id
+    end
+
+  end
 
   belongs_to :user
   has_one :purchase
@@ -7,3 +23,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :status, :delivery_fee, :delivery_zone, :delivery_time
 end
+
+
+
+
