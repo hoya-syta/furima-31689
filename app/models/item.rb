70d50e8a83246db5eaebd3a8daa1d1db7570f2b2
hoya-syta <erukhnjhgfd@gmail.com>
2020-11-23 18:15:14 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :status, :delivery_fee, :delivery_zone, :delivery_time
 end
---
+++ spec/factories/delivery_zones.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :delivery_zone do
+    
+  end
+end
