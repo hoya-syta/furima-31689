0e09a88d5ae729d5ad0e06de3b1ed129cdfe8443
hoya-syta <erukhnjhgfd@gmail.com>
2020-12-01 18:57:44 +0900

    テストコード実装

--- spec/factories/address_purchases.rb
+++ spec/factories/address_purchases.rb
@@ -1,12 +1,11 @@
 FactoryBot.define do
   factory :address_purchase do
-    postal_code
-    prefectures_id
-    municipalities
-    address
-    house_number
-    phone_number
+    postal_code     {'123-4567'}
+    prefectures_id  {'3'}
+    municipalities  {'渋谷区'}
+    address         {'青山1-1'}
+    house_number    {'青山ビル1-2'}
+    phone_number    {'09012345678'}
     
-
   end
 end
--- spec/models/address_purchase_spec.rb
+++ spec/models/address_purchase_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe AddressPurchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '購入者情報の保存' do
+    before do
+      @address_purchase = FactoryBot.build(:address_purchase)
+    end
+
+    it 'すべての値が正しく入力されていれば保存できること' do
+      expect(@address_purchase).to be_valid
+    end
+
+    it 'postal_codeが空だと保存できないこと' do
+      @address_purchase.postal_code = nil
+      @address_purchase.valid?
+      expect(@address_purchase.errors.full_messages).to include("Postal code can't be blank")
+    end
+    it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+      @address_purchase.postal_code = '１２３４５６７'
+      @address_purchase.valid?
+      expect(@address_purchase.errors.full_messages).to include("Postal code is invalid")
+    end
+    it 'prefectures_idが1では登録できないこと' do
+      @address_purchase.prefectures_id = '1'
+      @address_purchase.valid?
+      expect(@address_purchase.errors.full_messages).to include('Prefectures must be other than 1')
+    end
+    it 'municipalitiesが空だと保存できないこと' do
+      @address_purchase.municipalities = nil
+      @address_purchase.valid?
+      expect(@address_purchase.errors.full_messages).to include("Municipalities can't be blank")
+    end
+    it 'addressが空だと保存できないこと' do
+      @address_purchase.address = nil
+      @address_purchase.valid?
+      expect(@address_purchase.errors.full_messages).to include("Address can't be blank")
+    end
+    it 'house_numberは空でも保存できること' do
+      @address_purchase.house_number = nil
+      expect(@address_purchase).to be_valid
+    end
+    it 'phone_numberが空だと保存できないこと' do
+      @address_purchase.phone_number = nil
+      @address_purchase.valid?
+      expect(@address_purchase.errors.full_messages).to include("Phone number can't be blank")
+    end
+    it 'phone_numberにはハイフンは不要で、11桁以内でないと保存できないこと' do
+      @address_purchase.phone_number = '０９０-１２３-４５６７８'
+      @address_purchase.valid?
+      expect(@address_purchase.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)", "Phone number is invalid")
+    end
+  end
 end
+
